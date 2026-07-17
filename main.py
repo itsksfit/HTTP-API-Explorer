a8c0eb28c098de935463912bd7230c24fc1ca2cc
itsksfit <ks1445674@gmail.com>
2026-07-17 23:10:01 +0530

    feat: parse and display API response

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from api.client import get_request
+
+
 def main():
     print("=" * 40)
     print("HTTP API Explorer")
@@ -8,9 +10,26 @@ def main():
 
     print("\nSending Request...\n")
 
-    response = get_request(url)
+    response, response_time = get_request(url)
 
+    if response is None:
+        return
+
+    print(f"Response Time : {response_time:.2f} seconds")
     print(f"Status Code : {response.status_code}")
+
+    print("\nResponse:\n")
+
+    data = response.json()
+
+    print(f"Current User URL : {data.get('current_user_url', 'N/A')}")
+    print(f"Repository Search : {data.get('repository_search_url', 'N/A')}")
+    print(f"Repository URL : {data.get('repository_url', 'N/A')}")
+
+    print("\nHeaders:\n")
+
+    for key, value in response.headers.items():
+        print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
